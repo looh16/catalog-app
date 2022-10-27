123303acf8bc75bda0ff6d928542a6bd7893c289
pamphilemkp <pamphilemkp@gmail.com>
2022-10-27 15:29:07 +0300

    add tables

--- schema.sql
+++ schema.sql
@@ -45,3 +45,25 @@ CREATE TABLE item (
     FOREIGN KEY (label_id) REFERENCES label (id),
     FOREIGN KEY (genre_id) REFERENCES genre (id)
 );
+----------TABLE BOOK----------------
+CREATE TABLE book
+(
+  publisher    VARCHAR(255),
+  cover_state    VARCHAR(255)
+) INHERITS (item);
+
+----------TABLE GAME----------------
+CREATE TABLE game(
+  multiplayer VARCHAR(255),
+  last_played_at DATE
+)INHERITS (item);
+
+----------TABLE MUSIC ALBUM----------------
+CREATE TABLE music_albums(
+  on_spotify BOOLEAN
+)INHERITS (item);
+
+----------TABLE MOVIE----------------
+CREATE TABLE movie(
+  silent BOOLEAN
+)INHERITS (item);
