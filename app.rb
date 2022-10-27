42f6d7754617eb4ae59918e4632651b95784d8be
pamphilemkp <pamphilemkp@gmail.com>
2022-10-27 12:05:08 +0300

    fix merging conflict

--- app.rb
+++ app.rb
@@ -1,9 +1,7 @@
-
 require_relative './data/all_music_albums'
 require './data/add_music_album'
 require_relative './data/add_movies'
 require_relative './data/list_movies'
-
 
 class App
   def start_app
@@ -11,7 +9,7 @@ class App
     case output
 
     when 2 then AllMusicAlbums.new.all_music_albums
-     when 3 then list_movies
+    when 3 then list_movies
     when 10 then AddMusicAlbum.new.add_music_album
     when 11 then add_movie
     when 13
